6276555f372a3265da8791fe70c544e2948563e4
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-04-13 10:53:12 +0300

    feat: add CsvWriterPipeline

--- scrapy_jobs/pipelines.py
+++ scrapy_jobs/pipelines.py
@@ -2,12 +2,24 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import scrapy
+from scrapy.exporters import CsvItemExporter
 
 
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+class CsvWriterPipeline:
+    def __init__(self) -> None:
+        self.file = None
+        self.exporter = None
 
+    def open_spider(self, spider: scrapy.Spider) -> None:
+        self.file = open("jobs.csv", "wb")
+        self.exporter = CsvItemExporter(self.file)
+        self.exporter.start_exporting()
 
-class ScrapyJobsPipeline:
-    def process_item(self, item, spider):
+    def close_spider(self, spider: scrapy.Spider) -> None:
+        self.exporter.finish_exporting()
+        self.file.close()
+
+    def process_item(self, item: scrapy.Item, spider: scrapy.Spider):
+        self.exporter.export_item(item)
         return item
